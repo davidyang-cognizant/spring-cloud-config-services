a569a361f34d5c8267d92fdd09ab96988956a2ca
David Yang <davidyang.cognizant@gmail.com>
2022-09-26 17:30:56 -0500

    Initial commit

---
+++ src/test/java/com/example/springcloudconfigservices/SpringCloudConfigServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
