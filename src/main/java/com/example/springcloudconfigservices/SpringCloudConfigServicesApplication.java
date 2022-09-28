ec3b4dbaebb8585a30847b0f1bc5878345ea136a
David <yangda@msoe.edu>
2022-09-27 23:02:39 -0500

    c

--- src/main/java/com/example/springcloudconfigservices/SpringCloudConfigServicesApplication.java
+++ src/main/java/com/example/springcloudconfigservices/SpringCloudConfigServicesApplication.java
@@ -1,11 +1,14 @@
 package com.example.springcloudconfigservices;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-@EnableConfigServer
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServicesApplication {
 
 	public static void main(String[] args) {
